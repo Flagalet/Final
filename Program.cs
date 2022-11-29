0d18ed96ec91347773ca3580b1168d2dccc62715
Dmitriy <zeybert90@mail.ru>
2022-11-29 23:22:52 +0300

    \2

--- Program.cs
+++ Program.cs
@@ -31,11 +31,12 @@ string[] CheckArray(string[] array)
 
 void PrintArray(string [] array)
 {
+    Console.Write($"[");
     for(int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]}");
     }
-    Console.WriteLine();
+    Console.Write($"]");
 }
 
 Console.Clear();
